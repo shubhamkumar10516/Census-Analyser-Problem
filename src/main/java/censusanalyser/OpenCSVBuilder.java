953d3abc24b7e6e1ad3b54d405f41bd9e43de7bb
shubham <shubhamkumar10516@gmail.com>
2020-10-26 00:51:28 +0530

    [Shubham]REFACTORED: Returning CSV file as list

--- src/main/java/censusanalyser/OpenCSVBuilder.java
+++ src/main/java/censusanalyser/OpenCSVBuilder.java
@@ -2,21 +2,35 @@ package censusanalyser;
 
 import java.io.Reader;
 import java.util.Iterator;
+import java.util.List;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
-public class OpenCSVBuilder<E> implements CSVBuilder<E> {
+public class OpenCSVBuilder implements CSVBuilder {
 
-	public Iterator<E> getCSVFileIterator(Reader reader, Class<E> csvClass) throws CensusAnalyserException {
-		try { 
-			CsvToBeanBuilder<E> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
-			csvToBeanBuilder.withType(csvClass);
-			csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
-			CsvToBean<E> csvToBean = csvToBeanBuilder.build();
-			return  csvToBean.iterator();
+	@Override
+	public <E> List<E> getCSVFileList(Reader reader, Class<E> csvClass) throws CensusAnalyserException {
+		return getCsvToBean(reader, csvClass).parse();
+	}
+
+	@Override
+	public <E> Iterator<E> getCSVFileIterator(Reader reader, Class<E> csvClass) throws CensusAnalyserException {
+		try {
+			return getCsvToBean(reader, csvClass).iterator();
 		} catch (IllegalStateException e) {
 			throw new CensusAnalyserException(e.getMessage(), CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE);
 		}
 	}
 
+	private <E> CsvToBean<E> getCsvToBean(Reader reader, Class<E> csvClass) throws CensusAnalyserException {
+		try {
+			CsvToBeanBuilder<E> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+			csvToBeanBuilder.withType(csvClass);
+			csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+			CsvToBean<E> csvToBean = csvToBeanBuilder.build();
+			return csvToBean;
+		} catch (IllegalStateException e) {
+			throw new CensusAnalyserException(e.getMessage(), CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE);
+		}
+	}
 }
