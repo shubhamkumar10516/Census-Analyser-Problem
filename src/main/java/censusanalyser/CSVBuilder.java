9cd5379b098fb0efc42dd5bd26f7ecbb10b88208
shubham <shubhamkumar10516@gmail.com>
2020-10-26 00:56:15 +0530

    [Shubham]REFACTORED: Getting CSVFile as List abstract method added

--- src/main/java/censusanalyser/CSVBuilder.java
+++ src/main/java/censusanalyser/CSVBuilder.java
@@ -2,9 +2,10 @@ package censusanalyser;
 
 import java.io.Reader;
 import java.util.Iterator;
+import java.util.List;
 
-public interface CSVBuilder<E> {
+public interface CSVBuilder {
 
-	public Iterator<E>  getCSVFileIterator(Reader reader, Class<E> csvClass) throws CensusAnalyserException;
-		
+	public<E> Iterator<E>  getCSVFileIterator(Reader reader, Class<E> csvClass) throws CensusAnalyserException;
+	public<E> List<E> getCSVFileList(Reader reader, Class<E> csvClass) throws CensusAnalyserException;
 }
